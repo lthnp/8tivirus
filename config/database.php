20df6854375128fdbc1e609fc76a34d18f391775
lthnp <59081454+lthnp@users.noreply.github.com>
2021-07-01 01:25:00 +0700

    readme and set driver

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
             'unix_socket' => '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => 'uat',
+            'prefix' => 'uat_',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
